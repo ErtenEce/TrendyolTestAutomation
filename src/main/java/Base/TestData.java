9bae6f73387b196d922d1d2a5377cec3a61ee9ea
Ece ERTEN <ertenece@outlook.com>
2024-10-07 17:57:55 +0300

    Added project

---
+++ src/main/java/Base/TestData.java
@@ -0,0 +1,9 @@
+package Base;
+
+public class TestData {
+
+  public String mail = "mugen.dai019@gmail.com";
+  public String password ="Bk12345678";
+  public String url = "https://www.trendyol.com/giris";
+
+}
